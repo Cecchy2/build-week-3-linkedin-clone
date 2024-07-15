05ca472892025892d0542d6342e7e1afd9ff0780
Cecchy2 <dariocecchinato@gmail.com>
2024-07-15 17:19:27 +0200

    aggiunto collapse ed animazione per fare scorrere il menu in basso

--- src/components/MessageComp.jsx
+++ src/components/MessageComp.jsx
@@ -1,76 +1,74 @@
-import Dropdown from "react-bootstrap/Dropdown";
-import NavItem from "react-bootstrap/NavItem";
-import NavLink from "react-bootstrap/NavLink";
-import { RiArrowUpWideLine } from "react-icons/ri";
+import React, { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { PiNotePencilDuotone } from "react-icons/pi";
-import { Col, Container, Row } from "react-bootstrap";
+import { RiArrowUpWideLine } from "react-icons/ri";
+import { Col, Container, Row, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MessageComp() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="d-inline-block position-fixed border rounded-top p-2" id="messageTool">
-      <Dropdown as={NavItem} drop="up">
-        <div className="d-flex justify-content-around align-items-center top-bar">
-          <img
-            src="https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="immagine user"
-            className="border rounded-circle me-2"
-            style={{ width: "35px" }}
-          />
-          <Dropdown.Toggle as={NavLink}>
-            <span className="fs-6 fw-medium">Messagistica</span>
-          </Dropdown.Toggle>
-          <BsThreeDots className="ms-5" size={17} />
-          <PiNotePencilDuotone className="ms-3" size={15} />
-          <RiArrowUpWideLine className="ms-3" size={20} />
-        </div>
-        <Dropdown.Menu className="mb-3 dropdown-menu-custom">
-          <Dropdown.Item className="messageDropDown p-0">
-            <Container>
-              <Row className="align-items-center">
-                <Col xs="auto">
-                  <img
-                    src="https://plus.unsplash.com/premium_photo-1664199486611-3e1277e150cd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="immagine utente"
-                    className="border rounded-circle img-fluid ms-1"
-                    style={{ width: "50px", height: "50px" }}
-                  />
-                </Col>
-                <Col className="messageData">
-                  <p className="mb-0 truncate msgTxt">NOME COGNOME</p>
-                  <p className="mb-0 truncate msgTxt">NOME: messaggio default dell' utente</p>
-                </Col>
-                <Col xs="auto">
-                  <p className="msgTxt me-1">15 Luglio 2024</p>
-                </Col>
-              </Row>
-            </Container>
-          </Dropdown.Item>
+    <div className={`messageTool ${open ? "open" : ""}`}>
+      <div className="d-flex justify-content-around align-items-center top-bar">
+        <img
+          src="https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="immagine user"
+          className="border rounded-circle me-2"
+          style={{ width: "35px" }}
+        />
+        <span className="fs-6 fw-medium">Messagistica</span>
+        <BsThreeDots className="ms-5" size={17} />
+        <PiNotePencilDuotone className="ms-3" size={15} />
+        <Button
+          variant="link"
+          className="p-0 ms-3"
+          onClick={() => setOpen(!open)}
+          aria-controls="message-collapse"
+          aria-expanded={open}
+        >
+          <RiArrowUpWideLine size={20} />
+        </Button>
+      </div>
+      <div id="message-collapse" className="dropdown-menu-custom">
+        <Container>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1664199486611-3e1277e150cd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="immagine utente"
+                className="border rounded-circle img-fluid ms-1"
+                style={{ width: "50px", height: "50px" }}
+              />
+            </Col>
+            <Col className="messageData">
+              <p className="mb-0 truncate msgTxt">NOME COGNOME</p>
+              <p className="mb-0 truncate msgTxt">NOME: messaggio default dell'utente</p>
+            </Col>
+            <Col xs="auto">
+              <p className="msgTxt me-1">15 Luglio 2024</p>
+            </Col>
+          </Row>
           <hr className="w-75 ms-5" />
-          <Dropdown.Item className="messageDropDown p-0">
-            <Container>
-              <Row className="align-items-center">
-                <Col xs="auto">
-                  <img
-                    src="https://plus.unsplash.com/premium_photo-1664199486611-3e1277e150cd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="immagine utente"
-                    className="border rounded-circle img-fluid"
-                    style={{ width: "50px", height: "50px" }}
-                  />
-                </Col>
-                <Col className="messageData">
-                  <p className="mb-0 truncate msgTxt">NOME COGNOME</p>
-                  <p className="mb-0 truncate msgTxt">NOME: messaggio default dell'utente</p>
-                </Col>
-                <Col xs="auto">
-                  <p className="msgTxt me-1">15 Luglio 2024</p>
-                </Col>
-              </Row>
-            </Container>
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <img
+                src="https://plus.unsplash.com/premium_photo-1664199486611-3e1277e150cd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="immagine utente"
+                className="border rounded-circle img-fluid ms-1"
+                style={{ width: "50px", height: "50px" }}
+              />
+            </Col>
+            <Col className="messageData">
+              <p className="mb-0 truncate msgTxt">NOME COGNOME</p>
+              <p className="mb-0 truncate msgTxt">NOME: messaggio default dell'utente</p>
+            </Col>
+            <Col xs="auto">
+              <p className="msgTxt me-1">15 Luglio 2024</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
