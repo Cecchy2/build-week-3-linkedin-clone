8ee090a650e3675eb6a00bdd1795523ecaa4d19e
Andrea <ageria20@gmail.com>
2024-07-15 16:30:41 +0200

    main section finita - da sistemare img profilo

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -1,11 +1,30 @@
-import { Button, Col, Container, Image, Row } from "react-bootstrap";
+import { Button, Col, Container, Image, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 
 const MainProfile = () => {
   return (
     <>
-      <Container className="badgeContainer border rounded-3 my-3">
-        <Image className="img-fluid h-25" src="../assets/Business LinkedIn Banner.png" alt="profile banner" />
+      <Container className="badgeContainer border rounded-3 my-3 px-0 " style={{ overflow: "hidden" }}>
+        <div className="position-relative">
+          <Image
+            className=" w-100"
+            src="https://media.licdn.com/dms/image/D4D16AQFsGlm6VDoeXg/profile-displaybackgroundimage-shrink_350_1400/0/1720601266129?e=1726704000&v=beta&t=4S9lvM6oCsEmZvwmBWICewK5cjTLixOITQDewadEhug"
+            alt="profile banner"
+            style={{ maxHeight: "25vh" }}
+          />
+          <Image
+            className="rounded-circle position-absolute mb-3"
+            src="https://media.licdn.com/dms/image/D4D03AQEsFXt1c56YnQ/profile-displayphoto-shrink_200_200/0/1715036214765?e=1726704000&v=beta&t=C9wJQFQf0myJr_2j3b9YQuVIbQVwcKtaTPbaiNQyNzI"
+            style={{ top: "100px", left: "50px" }}
+          />
+        </div>
         <Container fluid>
+          <div className="mt-5 w-50 ms-auto d-flex justify-content-end">
+            <Image
+              src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgaWQ9ImVkaXQtbWVkaXVtIiBhcmlhLWhpZGRlbj0idHJ1ZSIgcm9sZT0ibm9uZSIgZGF0YS1zdXBwb3J0ZWQtZHBzPSIyNHgyNCIgZmlsbD0iY3VycmVudENvbG9yIj4KICA8cGF0aCBkPSJNMjEuMTMgMi44NmEzIDMgMCAwMC00LjE3IDBsLTEzIDEzTDIgMjJsNi4xOS0yTDIxLjEzIDdhMyAzIDAgMDAwLTQuMTZ6TTYuNzcgMTguNTdsLTEuMzUtMS4zNEwxNi42NCA2IDE4IDcuMzV6Ii8+Cjwvc3ZnPg=="
+              width="25"
+              height="25"
+            />
+          </div>
           <h2>Andrea Geria</h2>
           <Row>
             <Col xs={12} md={8}>
@@ -49,7 +68,7 @@ const MainProfile = () => {
         </Container>
       </Container>
       <Container className="border rounded-3 my-3">
-        <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex justify-content-between align-items-center pt-3">
           <h4>Esperienza</h4>
           <button className="bg-transparent border-0">
             <svg
@@ -87,6 +106,107 @@ const MainProfile = () => {
             </Col>
           </Row>
         </Container>
+      </Container>
+      <Container className="border rounded-3 my-3">
+        <div className="d-flex justify-content-between align-items-center pt-3">
+          <h4>Formazione</h4>
+          <button className="bg-transparent border-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              data-supported-dps="24x24"
+              fill="currentColor"
+              className="mercado-match"
+              width="24"
+              height="24"
+              focusable="false"
+            >
+              <path d="M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8z"></path>
+            </svg>
+          </button>
+        </div>
+        <Container className="border-bottom p-0">
+          <div className="p-3">
+            <Row>
+              <Col xs={1} className="p-0 ">
+                <img
+                  width="55"
+                  src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1729123200&amp;v=beta&amp;t=h5xweoh6ztkgY0_oRfROE4Q649H11tcWlMMnHpR8qok"
+                  loading="lazy"
+                  height="55"
+                  alt="Logo di EPICODE"
+                  id="ember2008"
+                  className="ms-auto"
+                ></img>
+              </Col>
+              <Col xs={11} className="p-0">
+                <h5 className="mb-0 px-0">School</h5>
+                <p className="mb-0">Where</p>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </Container>
+      <Container className="border rounded-3 my-3">
+        <div className="d-flex justify-content-between align-items-center pt-3">
+          <h4>Competenze</h4>
+          <button className="bg-transparent border-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              data-supported-dps="24x24"
+              fill="currentColor"
+              className="mercado-match"
+              width="24"
+              height="24"
+              focusable="false"
+            >
+              <path d="M21 13h-8v8h-2v-8H3v-2h8V3h2v8h8z"></path>
+            </svg>
+          </button>
+        </div>
+        <ListGroup>
+          <ListGroupItem className="border-start-0 border-top-0 border-end-0 py-3">
+            <Container>
+              <Row>
+                <Col xs={1} className="p-0 ">
+                  <img
+                    width="45"
+                    src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1729123200&amp;v=beta&amp;t=h5xweoh6ztkgY0_oRfROE4Q649H11tcWlMMnHpR8qok"
+                    loading="lazy"
+                    height="45"
+                    alt="Logo di EPICODE"
+                    id="ember2008"
+                    className="ms-auto"
+                  ></img>
+                </Col>
+                <Col xs={11} className="p-0">
+                  <p className="mb-0">Competenza #1</p>
+                </Col>
+              </Row>
+            </Container>
+          </ListGroupItem>
+          <ListGroupItem className="border-0 py-3">
+            <Container>
+              <Row>
+                <Col xs={1} className="p-0 ">
+                  <img
+                    width="45"
+                    src="https://media.licdn.com/dms/image/C4E0BAQHYgix-Ynux1A/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1729123200&amp;v=beta&amp;t=h5xweoh6ztkgY0_oRfROE4Q649H11tcWlMMnHpR8qok"
+                    loading="lazy"
+                    height="45"
+                    alt="Logo di EPICODE"
+                    id="ember2008"
+                    className="ms-auto"
+                  ></img>
+                </Col>
+                <Col xs={11} className="p-0">
+                  <p className="mb-0">Competenza #2</p>
+                </Col>
+              </Row>
+            </Container>
+          </ListGroupItem>
+        </ListGroup>
       </Container>
     </>
   );
