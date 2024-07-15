3365af14a079ca9075a19042339e1f7e65f9c47b
Cecchy2 <dariocecchinato@gmail.com>
2024-07-15 10:53:40 +0200

    impostati reducers e store

---
+++ src/redux/reducers/profileReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  user: {
+    name: "",
+    surname: "",
+    email: "",
+    username: "",
+    bio: "",
+    title: "",
+    area: "",
+  },
+};
+
+const profileReducer = (state = initialState, action) => {};
+
+export default profileReducer;
