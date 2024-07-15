17e5633a841909ccf650d2e751f33bd21e8a6e6d
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-07-15 15:16:38 +0200

    Commit
    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React from 'react';
+import MyNavbar from './components/MyNavbar';
+import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
 }
 
 export default App;
