40e50b1148998c342bc3e87288642fcf8302adc3
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-07-16 11:43:15 +0200

    npm i icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 import MainProfile from "./components/MainProfile";
 import Aside from "./components/AsideProfile";
 import MyNavbar from "./components/MyNavbar";
 import MessageComp from "./components/MessageComp";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
