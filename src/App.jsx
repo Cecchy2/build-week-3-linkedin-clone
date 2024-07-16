534277379b327404f85da263a7973d685d0cdd06
Andrea <ageria20@gmail.com>
2024-07-16 10:43:06 +0200

    home page sistemata

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MainProfile from "./components/MainProfile";
-import Aside from "./components/AsideProfile"
-import MyNavbar from './components/MyNavbar';
+import Aside from "./components/AsideProfile";
+import MyNavbar from "./components/MyNavbar";
+import { Col, Container, Row } from "react-bootstrap";
 function App() {
   return (
     <>
-    <MyNavbar />
-      <MainProfile />
-      
-      <Aside/>
+      <MyNavbar />
+      <Container>
+        <Row>
+          <Col xs={12} md={8}>
+            <MainProfile />
+          </Col>
+          <Col xs={12} md={2}>
+            <Aside />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
