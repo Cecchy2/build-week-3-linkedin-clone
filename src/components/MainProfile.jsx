4e8e751e53e00fa51268a790178051989584508f
Cecchy2 <dariocecchinato@gmail.com>
2024-07-17 18:34:13 +0200

    refactor modale userEdit

--- src/components/MainProfile.jsx
+++ src/components/MainProfile.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { editFetchProfile, editUserAction, getProfileMe, uploadProfilePicture } from "../redux/actions";
 import { createExp, getExp } from "../redux/actions";
 import ModalProfilePicture from "./ModalProfilePicture";
+import ModalUserEdit from "./ModalUserEdit";
 
 const MainProfile = () => {
   const [showEdit, setShowEdit] = useState(false);
@@ -140,100 +141,13 @@ const MainProfile = () => {
                   height="25"
                 />
               </Button>
-              <Modal show={showEdit} onHide={handleCloseEdit}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Modifica presentazione</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <p className="text-muted fs-6">
-                    <span>*</span>indica che è obbligatorio
-                  </p>
-                  <Form onSubmit={handleFormSubmit}>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                      <Form.Label>Nome*</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="nome"
-                        autoFocus
-                        name="name"
-                        value={inputValue.name}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-                      <Form.Label>Cognome*</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="cognome"
-                        autoFocus
-                        name="surname"
-                        value={inputValue.surname}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        type="email"
-                        placeholder="name@example.com"
-                        autoFocus
-                        name="email"
-                        value={inputValue.email}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-                      <Form.Label>Username</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="username"
-                        autoFocus
-                        name="username"
-                        value={inputValue.username}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
-                      <Form.Label>Bio*</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="bio"
-                        autoFocus
-                        name="bio"
-                        value={inputValue.bio}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
-                      <Form.Label>Title*</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="title"
-                        autoFocus
-                        name="title"
-                        value={inputValue.title}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput7">
-                      <Form.Label>Area</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="area"
-                        autoFocus
-                        name="area"
-                        value={inputValue.area}
-                        onChange={handleInputChange}
-                      />
-                    </Form.Group>
-                    <Modal.Footer>
-                      <Button className="rounded-5 px-3" variant="primary" onClick={handleCloseEdit} type="submit">
-                        Save
-                      </Button>
-                    </Modal.Footer>
-                  </Form>
-                </Modal.Body>
-              </Modal>
+              <ModalUserEdit
+                showEdit={showEdit}
+                handleCloseEdit={handleCloseEdit}
+                handleFormSubmit={handleFormSubmit}
+                inputValue={inputValue}
+                handleInputChange={handleInputChange}
+              />
             </div>
             <h2>
               {profileMe.name} {profileMe.surname}
