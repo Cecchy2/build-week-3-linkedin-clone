edc6430170b8b85b745481234fa0227a04a302f5
Andrea <ageria20@gmail.com>
2024-07-18 16:52:31 +0200

    funzionalita' edit e delete solo per il profilo proprietario

--- src/components/HomeMain.jsx
+++ src/components/HomeMain.jsx
@@ -38,22 +38,28 @@ const HomeMain = () => {
                     <Card.Title>{post.username}</Card.Title>
                   </Container>
                   <Container className="p-0 d-flex justify-content-end">
-                    <ModalPostEdit userId={post._id} />
-                    <Button className="bg-transparent border border-0">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="15"
-                        height="15"
-                        fill="black"
-                        className="bi bi-trash-fill"
-                        viewBox="0 0 16 16"
-                        onClick={() => {
-                          dispatch(deletePost(post._id));
-                        }}
-                      >
-                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                      </svg>
-                    </Button>
+                    {profileMe._id === post.user._id ? (
+                      <>
+                        <ModalPostEdit userId={post._id} />
+                        <Button className="bg-transparent border border-0">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="15"
+                            height="15"
+                            fill="black"
+                            className="bi bi-trash-fill"
+                            viewBox="0 0 16 16"
+                            onClick={() => {
+                              dispatch(deletePost(post._id));
+                            }}
+                          >
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                          </svg>
+                        </Button>
+                      </>
+                    ) : (
+                      <></>
+                    )}
                   </Container>
                 </Container>
                 <Card.Text>{post.text}</Card.Text>
