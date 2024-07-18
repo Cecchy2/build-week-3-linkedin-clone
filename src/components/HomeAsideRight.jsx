c061d115cd378359d34dea5ce56b6288b6c4a941
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-07-18 10:20:57 +0200

    footer

--- src/components/HomeAsideRight.jsx
+++ src/components/HomeAsideRight.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Col, Button } from "react-bootstrap";
+import Footer from "./Footer"
 
 const Notizie = () => {
   const [mostraAltro, setMostraAltro] = useState(false);
@@ -64,20 +65,7 @@ const Notizie = () => {
           {mostraAltro ? "Nascondi altro" : "Mostra altro"}
         </Button>
       </div>
-      <div className="container-info">
-      <ul className="info-list">
-      <li>Informazioni</li>
-    <li>Accessibilità</li>
-    <li>Centro assistenza</li>
-    <li>Privacy e condizioni</li>
-    <span>Opzioni per gli annunci pubblicitari</span> <br></br>
-    <li>Pubblicità</li>
-    <li>Servizi alle aziende</li>
-    <li>Scarica l’app LinkedIn</li>
-    <li>Altro </li>
-    <span> <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/LinkedIn_logo.svg" style={{width:"70px"}}/> LinkedIn Corporation © 2024 </span>
-      </ul>
-      </div>
+      <Footer/>
     </>
   );
 };
