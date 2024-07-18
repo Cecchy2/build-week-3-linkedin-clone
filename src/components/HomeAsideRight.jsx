9f69c23d3cc87fef7476256255e256ea39dce785
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-07-18 09:59:10 +0200

    info list

--- src/components/HomeAsideRight.jsx
+++ src/components/HomeAsideRight.jsx
@@ -5,7 +5,7 @@ const Notizie = () => {
   const [mostraAltro, setMostraAltro] = useState(false);
 
   return (
-    <Col xs={{ span: 3 }}>
+    <>
       <div className="bg-white p-3 rounded" style={{ minWidth: "300px", boxShadow: " 0 2px 8px rgba(0, 0, 0, 0.1)" }}>
         <h4>
           LinkedIn Notizie{" "}
@@ -64,7 +64,21 @@ const Notizie = () => {
           {mostraAltro ? "Nascondi altro" : "Mostra altro"}
         </Button>
       </div>
-    </Col>
+      <div className="container-info">
+      <ul className="info-list">
+      <li>Informazioni</li>
+    <li>Accessibilità</li>
+    <li>Centro assistenza</li>
+    <li>Privacy e condizioni</li>
+    <span>Opzioni per gli annunci pubblicitari</span> <br></br>
+    <li>Pubblicità</li>
+    <li>Servizi alle aziende</li>
+    <li>Scarica l’app LinkedIn</li>
+    <li>Altro </li>
+    <span> <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/LinkedIn_logo.svg" style={{width:"70px"}}/> LinkedIn Corporation © 2024 </span>
+      </ul>
+      </div>
+    </>
   );
 };
 export default Notizie;
